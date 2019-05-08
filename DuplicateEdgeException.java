d756c135e87ebc75b00eb039bcc269bce9f27a05
srhodes <rhodestephane@comcast.net>
2019-05-07 17:57:36 -0700

    Add files via upload

---
+++ DuplicateEdgeException.java
@@ -0,0 +1,16 @@
+/*
+ *
+ * srhodes17@georgefox.edu
+ *
+ * Program 12
+ *
+ * 2018-12-14
+ *
+ * */
+
+/**
+ * Exception class for duplicate edges
+ */
+public class DuplicateEdgeException extends RuntimeException
+{
+}
