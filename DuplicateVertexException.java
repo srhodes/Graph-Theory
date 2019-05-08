aad991ac53caf7d11b199b00aefeac9bc4426abf
srhodes <rhodestephane@comcast.net>
2019-05-07 17:59:28 -0700

    Add files via upload

---
+++ DuplicateVertexException.java
@@ -0,0 +1,16 @@
+/*
+ *
+ * srhodes17@georgefox.edu
+ *
+ * Program 12
+ *
+ * 2018-12-14
+ *
+ * */
+
+/**
+ * Exception class for duplicate vertex
+ */
+public class DuplicateVertexException extends RuntimeException{
+
+}
